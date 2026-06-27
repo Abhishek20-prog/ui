23b91f8afd78f2b3715e59929dc27da93d136d3e
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-06-27 19:51:43 +0530

    initial commit

--- interface/src/components/Section1/Card.jsx
+++ interface/src/components/Section1/Card.jsx
@@ -4,6 +4,9 @@ const Card = () => {
   return (
     <div className=" bg-center bg-[url('https://images.unsplash.com/photo-1543428390-52250776dd46?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d29tZW4lMjBob2xkaW5nJTIwYW5kJTIwc2VlaW5nJTIwYSUyMHBob25lfGVufDB8fDB8fHww')]  border-2 border-amber-100 rounded-[25px] w-[95%]">
       <div className="bg-white flex justify-center items-center w-10 h-10 rounded-[50%] relative top-8 left-8" >1</div>
+      <div className="relative top-90 text-white flex justify-center items-center left-8 pr-10">Prime customers, that <br></br>have access to bank <br></br>credit and are satisfied <br></br>with the current product.
+
+</div>
     </div>
   )
 }
