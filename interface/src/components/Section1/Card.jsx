bdd469f0d555a53a670278829e78cb9ef2f14230
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-06-27 21:48:15 +0530

    initial commit

--- interface/src/components/Section1/Card.jsx
+++ interface/src/components/Section1/Card.jsx
@@ -9,7 +9,7 @@ const Card = () => {
 
 </div>
 <div className="flex">
-  <div className="bg-blue-600 h-11 w-[43%] relative left-13 top-120 rounded-[40px] flex text-[15px] text-white pt-2 pl-5">Satisfied</div>
+  <div className="bg-blue-600 h-11 w-[43%] relative left-13 top-120 rounded-[40px] flex text-[15px] text-white pt-2.5 pl-5">Satisfied</div>
 <div className="bg-blue-600 h-5 w-[17%] relative left-8 top-123 rounded-[40px]"></div>
 <button className="bg-blue-600 h-12 w-12 relative left-5 top-119 rounded-[50%] flex justify-center items-center hover:bg-blue-500 hover:pointer hover:scale-105 hover:shadow-lg"><img src={arrow} alt="arrow" className="w-8 h-8"/></button>
 </div>
